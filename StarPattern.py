099d06fadd3844b924b7e85c7c71ffeb322b61c8
GIGENDRA DAS T M <31034744+gigendra@users.noreply.github.com>
2021-08-27 18:42:59 +0530

    Add files via upload

---
+++ StarPattern.py
@@ -0,0 +1,19 @@
+/* program to print the belo pattern 
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+
+*/
+for i in range(0,10,1):
+    for i in range(0,i+1,1):
+        print(" ",end="")
+        print("*",end="")
+    print("") 
+      
